d87717e5b76fc461b36a6fe26e1af4e6678c7977
saniulsani <saniulislamsony311@gmail.com>
2023-04-07 17:12:55 +0600

    new again commit

---
+++ src/components/Grandpa/Grandpa.jsx
@@ -0,0 +1,35 @@
+import React, { createContext, useState } from 'react';
+import Father from '../Father/Father';
+import Uncle from '../Uncle/Uncle';
+import Aunty from '../Aunty/Aunty';
+import './Grandpa.css'
+export const RingContext = createContext('gold')
+export const MoneyContext = createContext(0)
+
+const Grandpa = () => {
+    const ring = 'diamond';
+    const [money, setMoney] = useState(0)
+    return (
+        <div className='grandpa'>
+            <h2>Grandpa</h2>
+            <p>Has Money: {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <RingContext.Provider value='golden Ring'>
+                    <section className='flex'>
+                        <Father ring={ring}></Father>
+                        <Uncle></Uncle>
+                        <Aunty ring={ring}></Aunty>
+                    </section>
+                </RingContext.Provider>
+            </MoneyContext.Provider>
+        </div>
+    );
+};
+
+export default Grandpa;
+
+/* 
+1. Create context
+2. Create a provider and pass a value
+3. use useContext to recifeve 
+*/
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import {
 import Main from './components/Layout/main';
 import Home from './components/Home/Home';
 import OrderReview from './components/OrderReview/OrderReview';
+import Grandpa from './components/Grandpa/Grandpa';
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
       {
         path: 'review',
         element: <OrderReview></OrderReview>
+      },
+      {
+        path: '/grandpa',
+        element: <Grandpa></Grandpa>
       }
     ]
   }
